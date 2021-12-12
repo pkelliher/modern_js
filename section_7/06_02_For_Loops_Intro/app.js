4152eccd2f99e92fc79857a7061ccc7633794b6a
pkelliher <pkelliher74@gmail.com>
2021-12-12 13:49:14 -0800

    for loop examples

--- section_7/06_02_For_Loops_Intro/app.js
+++ section_7/06_02_For_Loops_Intro/app.js
@@ -25,3 +25,33 @@ for (let i = 200; i >= 0; i -= 25) {
   console.log(i);
 }
 console.log("AFTER THE LOOP!");
+
+// Another perfect square for loop.
+for (let i = 1; i <= 20; i++) {
+  console.log(`Pefect squares: ${i}x${i} = ${i * i}`);
+}
+
+// decreenting by 1 for loop.
+for (let i = 10; i >= 0; i -= 1) {
+  console.log("Counting Backwards by 1:", i);
+}
+
+// decrementing by 2 for loop.
+for (let i = 20; i >= 0; i -= 2) {
+  console.log("Counting Backwards by 2", i);
+}
+
+// incrementing by 2 for loop.
+for (let i = 0; i <= 20; i += 2) {
+  console.log("Incrementing by 2", i);
+}
+
+// decrementing by 5 for loop.
+for (let i = 100; i >= 0; i -= 5) {
+  console.log("Counting Backwards by 5", i);
+}
+
+// incrementing by 5 for loop.
+for (let i = 0; i <= 100; i += 5) {
+  console.log("Incrementing by 5", i);
+}
