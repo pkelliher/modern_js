d4bc29e33c3ee1221b3446bf1835f225732b9ca3
pkelliher <pkelliher74@gmail.com>
2021-12-12 18:28:37 -0800

    adding arrays

--- section_8/07_08_Practice_2_Avg/app.js
+++ section_8/07_08_Practice_2_Avg/app.js
@@ -1,3 +1,15 @@
 // Write a function to find the average value in an array of numbers
 //avg([0,50]) //25
 //avg([75,76,80,95,100]) //85.2
+
+function avg(arr) {
+  let total = 0;
+  // loop over each num
+  for (let num of arr) {
+    //add them togeterher
+    total += num;
+  }
+  // divide by numer of nums
+  let res = total / arr.length;
+  return res;
+}
